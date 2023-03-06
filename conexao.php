ffd4f740344b4cb77719846c1d613b8babd3d793
NefiJL <116108711+NefiJL@users.noreply.github.com>
2023-03-05 22:56:38 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -7,13 +7,11 @@ $senha = "";
 
 $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
 
-if($mysqli ->connect_errno) {
+if ($mysqli->connect_errno) {
 
-    echo "Falha ao conectar (". $mysqli -> connect_errno . ") " . $msqli -> connect_errno; 
+    echo "Falha ao conectar (" . $mysqli->connect_errno . ") " . $msqli->connect_errno;
 
-}
-
-else
+} else
     echo "Conectado ao Banco de Dados";
 
 ?>
