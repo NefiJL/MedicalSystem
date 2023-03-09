80a9904bd65998d6a42cd4b078e777ab8c70af27
GabrielSantanaL <116109002+GabrielSantanaL@users.noreply.github.com>
2023-03-09 16:52:45 -0300

    Update log-out.php

--- log-out.php
+++ log-out.php
@@ -21,15 +21,11 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          
-        <li class="nav-item"><a class="nav-link" href="./login.php">
-           <span data-feather="log-out" style="color: white;"></span>&nbsp;&nbsp; Log-out</a>
+        <li class="nav-item"><a class="nav-link" style="margin-left: 1600px;" href="./login.php">
+           <span data-feather="log-out" style="color: white;"></span>&nbsp;Log-out</a>
           </li>
         </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+  
       </div>
     </div>
   </nav>
