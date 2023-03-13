3086e66c97a142481258d39787501e748ec6bb9b
NefiJL <116108711+NefiJL@users.noreply.github.com>
2023-03-12 23:29:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 </head>
 <header>
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
       <a class="navbar-brand" href="./index.php">MedicalSystem</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
