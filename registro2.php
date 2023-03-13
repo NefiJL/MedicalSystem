a9e6859e02c02028dbe6f6ffb80f71c439348caa
NefiJL <116108711+NefiJL@users.noreply.github.com>
2023-03-12 23:29:29 -0300

    Update registro2.php

--- registro2.php
+++ registro2.php
@@ -1,3 +1,60 @@
+<?php
+session_start();
+require_once 'conexao.php';
+
+$cidade = '';
+$especialidade = '';
+$crm = '';
+$telefone = '';
+$senha = '';
+
+
+// Verificar si se han enviado datos del formulario
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['cidade'])) {
+        $cidade = mysqli_real_escape_string($conexao, $_POST['cidade']);
+      }
+      
+      if (isset($_POST['especialidade'])) {
+        $especialidade = mysqli_real_escape_string($conexao, $_POST['especialidade']);
+      }
+      
+      if (isset($_POST['crm'])) {
+        $crm = mysqli_real_escape_string($conexao, $_POST['crm']);
+      }
+      
+      if (isset($_POST['telefone'])) {
+        $telefone = mysqli_real_escape_string($conexao, $_POST['telefone']);
+      }
+      
+      if (isset($_POST['senha'])) {
+        $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
+      }
+
+   // Preparar consulta de inserción de usuario
+   $stmt = mysqli_prepare($conexao, "INSERT INTO usuario (cidade, especialidade, crm, telefone, senha) VALUES (?, ?, ?, ?, ?)");
+   mysqli_stmt_bind_param($stmt, "sssss", $cidade, $especialidade, $crm, $telefone, $senha);
+ 
+   // Guardar datos en variables de sesión
+  $_SESSION['cidade'] = $cidade;
+  $_SESSION['especialidade'] = $especialidade;
+  $_SESSION['crm'] = $crm;
+  $_SESSION['telefone'] = $telefone;
+  $_SESSION['senha'] = $senha;
+
+   // Cerrar la conexión a la base de datos
+ mysqli_close($conexao);
+
+ // Cerrar la consulta preparada
+ mysqli_stmt_close($stmt);
+ 
+ 
+ exit();
+
+ }
+ 
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -22,55 +79,44 @@
 
 <body class=" text-center">
 
-    <form class="form-signin">
-
         <div class="backgroundR">
 
-            <form class="form-signin">
-
+        <form class="form-signin" method="POST">
                 <div class="inputContainer">
 
-                    <input type="text" id="inputCidade" class="input" placeholder="a" required autofocus>
+                    <input type="text" id="inputCidade" class="input" name="cidade" placeholder="a" required autofocus>
                     <label for="inputCidade" class="label">Cidade</label>
 
                 </div>
 
                 <div class="inputContainer">
 
-                    <input type="text" id="inputEspecialidade" class="input" placeholder="a" required>
+                    <input type="text" id="inputEspecialidade" class="input" name="especialidade" placeholder="a" required>
                     <label for="inputEspecialidade" class="label">Especialidade</label>
 
                 </div>
 
                 <div class="inputContainer">
 
-                    <input type="text" id="inputCRM" class="input" placeholder="a" required>
+                    <input type="text" id="inputCRM" class="input" name="crm" placeholder="a" required>
                     <label for="inputCRM" class="label">Numero de registro (CRM)</label>
 
                 </div>
 
                 <div class="inputContainer">
 
-                    <input type="tel" id="inputTel" class="input" placeholder="a" required>
-                    <label for="inpuTtel" class="label">Telefone</label>
+                    <input type="tel" id="inputTel" class="input" name="telefone" placeholder="a" required>
+                    <label for="inputTel"" class="label">Telefone</label>
 
                 </div>
 
 
                 <div class="inputContainer">
 
-                    <input type="password" id="inputSenhaR" class="input" placeholder="a" required>
+                    <input type="password" id="inputSenhaR" class="input" name="senha" placeholder="a" required>
                     <label for="inputSenhaR" class="label">Senha</label>
 
                 </div>
-
-                <div class="inputContainer">
-
-                    <input type="password" id="inputSenhaR2" class="input" placeholder="a" required>
-                    <label for="inputSenhaR2" class="label">Conforme a senha</label>
-
-                </div>
-
 
                 <br><button class=" btn btn-lg btn-primary btn-block" type="submit"
                     style="margin-top: 60px; width: 250px; height: 48px;">Continuar</button>
@@ -78,7 +124,6 @@
             </form>
 
             <div class="form-check" style="margin-top: -95px; margin-left: 18px;">
-
                 <input class="form-check-input" type="checkbox" id="flexCheckDefault">
                 <label class="form-check-label" for="flexCheckDefault">
                     Aceito os termos e condições
