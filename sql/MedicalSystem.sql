2cff406695e5dc6a94424bcbf3e42e2c0f8d6f8b
NefiJL <116108711+NefiJL@users.noreply.github.com>
2023-03-30 00:13:26 -0300

    Update MedicalSystem.sql

--- sql/MedicalSystem.sql
+++ sql/MedicalSystem.sql
@@ -4,13 +4,11 @@ CREATE TABLE usuario(
     id INT NOT NULL AUTO_INCREMENT,
  	nome varchar (50)NOT null,
     email varchar (50) NOT null,
-    cidade varchar (50) NOT null,
     especialidade varchar (50) NOT null,
     CRM varchar (15) NOT null,
-    telefone varchar (14) NOT null,
     senha varchar (20) NOT null,
     
     PRIMARY KEY(id)
 );
 
-INSERT INTO `usuario`(`Nome`, `Email`, `Cidade`, `Especialidade`, `CRM`, `Telefone`, `Senha`) VALUES ('Hector','hector@gmail.com','SC','Oftalmologia','00000101010','(47) 9 9285-8585','123')
+INSERT INTO `usuario`(`Nome`, `Email`, `Especialidade`, `CRM`, `Senha`) VALUES ('Hector','hector@gmail.com','Oftalmologia','00000101010','123')
