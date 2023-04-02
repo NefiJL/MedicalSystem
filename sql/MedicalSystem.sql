d7c53ba104165891d3458c242d0f044133e38947
NefiJL <116108711+NefiJL@users.noreply.github.com>
2023-04-01 22:23:49 -0300

    Update MedicalSystem.sql

--- sql/MedicalSystem.sql
+++ sql/MedicalSystem.sql
@@ -1,6 +1,5 @@
 CREATE TABLE usuario(
 
-
     id INT NOT NULL AUTO_INCREMENT,
  	nome varchar (50)NOT null,
     email varchar (50) NOT null,
@@ -12,3 +11,18 @@ CREATE TABLE usuario(
 );
 
 INSERT INTO `usuario`(`Nome`, `Email`, `Especialidade`, `CRM`, `Senha`) VALUES ('Hector','hector@gmail.com','Oftalmologia','00000101010','123')
+
+CREATE TABLE relato(
+
+    idR INT NOT NULL AUTO_INCREMENT,
+    altura VARCHAR(3) NOT NULL,
+    idade INT NOT NULL,
+    sexo ENUM('Masculino', 'm','M', 'f', 'M', 'Femenino') NOT NULL,
+    dataR DATE NOT NULL,
+    relato VARCHAR(10000) NOT NULL,
+    PRIMARY KEY(idR)
+
+);
+
+    UPDATE relato SET dataR = STR_TO_DATE(dataR, '%d/%m/%Y');
+
