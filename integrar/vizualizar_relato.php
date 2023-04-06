03b2d3066076b212ee7ba8ff96974b05c68ceb50
GabrielSantanaL <116109002+GabrielSantanaL@users.noreply.github.com>
2023-04-06 16:05:25 -0300

    Create vizualizar_relato.php

---
+++ integrar/vizualizar_relato.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset='utf-8'>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  <title>MedicalSystem</title>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="./css/styles.css">
+  <script src='./js/bootstrap.bundle.js'></script>
+</head>
+
+<header>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="./index.php">MedicalSystem</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item"><a class="nav-link" href="./login.php">
+              <span data-feather="user" style="color: white;"></span>&nbsp;&nbsp; Login</a>
+          </li>
+          <li class="nav-item"><a class="nav-link" href="./registro.php">
+              <span data-feather="user-plus" style="color: white;"></span>&nbsp;&nbsp; Registrar</a>
+          </li>
+        </ul>
+</header>
+
+<body>
+<div class="row coluna">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Título especial</h5>
+        <p class="card-text">Quando o osso está muito fora do lugar e não pode ser corrigido 
+          ou mantido na posição correta com o uso de gesso, é necessário fazer a cirurgia.
+          Dependendo da fratura, há várias opções para manter os ossos na posição correta até a consolidação:
+          gesso; pinos metálicos (normalmente de aço inoxidável ou titânio); </p>
+      </div>
+    </div>
+  </div>
+
+
+  <script type="text/javascript" src="./js/bootstrap.bundle.js"></script>
+  <script type="text/javascript" src="./js/feather.mim.js"></script>
+  <script type="text/javascript" src="./js/jquery-3.6.3.min.js"></script>
+  <script type="text/javascript" src="./js/menu.js"></script>
+
+</body>
+
+</html>
