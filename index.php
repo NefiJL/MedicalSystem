63963ef49444453f38a51079f8d04f5e74b6b461
GabrielSantanaL <116109002+GabrielSantanaL@users.noreply.github.com>
2023-04-28 15:38:00 -0300

    Update index.php

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
       </div>
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="./img//img6.png" class="d-block w-100" salt="balbblablab">
+          <img src="./img/img6.png" class="d-block w-100" salt="foto1">
           <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
             aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
             <rect width="100%" height="100%" fill="#777" />
