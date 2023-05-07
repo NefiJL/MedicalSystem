140297d8ce4572fecf39a1682d2572cd089f1394
NefiJL <116108711+NefiJL@users.noreply.github.com>
2023-05-07 18:41:13 -0300

    Update busca.php

--- busca.php
+++ busca.php
@@ -1,10 +1,10 @@
 <?php
 
-include('protect.php');
+session_start();
+require_once 'conexao.php';
 
-if(!isset($_SESSION)){
-    session_start();
-}
+$sql = "SELECT * FROM relato";
+$all_relato = $conexao->query($sql);
 
 ?>
 
@@ -23,74 +23,84 @@ if(!isset($_SESSION)){
 
 <header>
 
-<nav class="navbar navbar-dark bg-dark fixed-top">
-  <div class="container-fluid">
-    
-    <a class="navbar-brand" href="#">
-              <?php
+  <nav class="navbar navbar-dark bg-dark fixed-top">
+    <div class="container-fluid">
+
+      <a class="navbar-brand" href="#">
+        <?php
               echo "<h3>" . $_SESSION["nome"] . "</h3>";
               ?>
+      </a>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="buscar" aria-label="Search ">
+        <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+      </form>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar"
+        aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar"
+        aria-labelledby="offcanvasDarkNavbarLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
+            aria-label="Close"></button>
+        </div>
+
+        <div class="offcanvas-body">
+          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+
+            <li class="nav-item"><a class="nav-link" href="relato.php">
+                <span data-feather="book" style="color: white;"></span>&nbsp;&nbsp;Relatar
               </a>
-    <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="buscar" aria-label="Search ">
-      <button class="btn btn-outline-success" type="submit">Pesquisar</button>
-    </form>
+            </li>
 
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
-        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-
-          <li class="nav-item"><a class="nav-link" href="relato.php">
-              <span data-feather="book" style="color: white;"></span>&nbsp;&nbsp;Relatar
+            <li class="nav-item"><a class="nav-link" href="meus_relatos.php">
+                <span data-feather="book-open" style="color: white;"></span>&nbsp;&nbsp;Meus Relatos
               </a>
-          </li>
+            </li>
 
-          <li class="nav-item"><a class="nav-link" href="meus_relatos.php">
-              <span data-feather="book-open" style="color: white;"></span>&nbsp;&nbsp;Meus Relatos
-              </a>
-          </li>
-
-          <li class="nav-item"><a class="nav-link" href="logout.php"><i class="fas fa-sign-out-alt nav_icon"></i>
-              <span data-feather="log-out" style="color: white;"></span>&nbsp;&nbsp;Sair</a>
-          </li>
+            <li class="nav-item"><a class="nav-link" href="logout.php"><i class="fas fa-sign-out-alt nav_icon"></i>
+                <span data-feather="log-out" style="color: white;"></span>&nbsp;&nbsp;Sair</a>
+            </li>
+        </div>
       </div>
     </div>
-  </div>
-</nav>
+  </nav>
 
 </header>
 
 <body id="body">
 
-<div div class="card border-dark text-bg-dark mb-3" style="max-width: 18rem; margin-top: 10%; margin-left: 3%">
-<div class="card-body">
-    <h5 class="card-title" style="text-align: center" name="titulo" id="tituloID"><b>Card title</b></h5>
-    <p class="card-text" name="texto" id="textoID">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item" name="altura" id="alturaID"><b>Altura:</b></li>
-    <li class="list-group-item"  name="peso" id="pesoID"><b>Peso:</b></li>
-    <li class="list-group-item" name="edad" id="edadID"><b>Edad:</b></li>
-    <li class="list-group-item" name="sexo" id="sexoID"><b>Sexo:</b></li>
-  </ul>
-  <div class="card-body" style="text-align: center">
-  <button class="btn btn-lg btn-primary btn-block" type="submit">Visualizar</button>
-  </div>
-</div>
-
-    <script type="text/javascript" src="./js/bootstrap.bundle.js"></script>
-    <script type="text/javascript" src="./js/feather.mim.js"></script>
-    <script type="text/javascript" src="./js/jquery-3.6.3.min.js"></script>
-    <script type="text/javascript" src="./js/menu.js"></script>
+  <main>
+  <?php
+    while($row = mysqli_fetch_assoc($all_relato)){
+  ?>
+    <div div class="card border-dark text-bg-dark mb-3" style="max-width: 18rem; margin-top: 10%; margin-left: 3%">
+      <div class="card-body">
+        <h5 class="card-title" style="text-align: center" name="titulo" id="tituloID"><b><?php echo $row["titulo"];?></b></h5>
+        <p class="card-text" name="relato" id="relatoID"><?php echo $row["relatoD"];?>.</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item" name="altura" id="alturaID"><b>Altura:</b><?php echo $row["altura"];?>Mt</li>
+        <li class="list-group-item" name="peso" id="pesoID"><b>Peso:</b><?php echo $row["peso"];?>Kg</li>
+        <li class="list-group-item" name="idade" id="IdadeID"><b>Idade:</b><?php echo $row["idade"];?>anos</li>
+        <li class="list-group-item" name="sexo" id="sexoID"><b>Sexo:</b><?php echo $row["sexo"];?></li>
+      </ul>
+      <div class="card-body" style="text-align: center">
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Visualizar</button>
+      </div>
+    </div>
+  <?php
+    }
+  ?> 
+  </main>
+  <script type="text/javascript" src="./js/bootstrap.bundle.js"></script>
+  <script type="text/javascript" src="./js/feather.mim.js"></script>
+  <script type="text/javascript" src="./js/jquery-3.6.3.min.js"></script>
+  <script type="text/javascript" src="./js/menu.js"></script>
 
 </body>
 
